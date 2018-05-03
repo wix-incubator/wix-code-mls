4172bf5685092793e43e9b471ddf1ad365f86c36
yoav@wix.com <yoav@wix.com>
2018-05-03 20:51:01 +0300

    refactoring of the example

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var rets = require('rets-client');
-var fs = require('fs');
-var photoSourceId = '12345'; // <--- dummy example ID!  this will usually be a MLS number / listing id
+const rets = require('rets-client');
+const fs = require('fs');
+const util = require('util');
+const clientSettings = require("./josh-credentials").clientSettings;
 
-import {clientSettings} from "./josh-credentials";
+const photoSourceId = '12345'; // <--- dummy example ID!  this will usually be a MLS number / listing id
 
 function outputFields(obj, opts) {
   if (!obj) {
@@ -36,114 +37,163 @@ function outputFields(obj, opts) {
   console.log("");
 }
 
+function log(obj) {
+  console.log(util.inspect(obj, {colors: true, depth: 5}));
+}
+
+async function getResources() {
+  let data = await client.metadata.getResources();
+
+  console.log("======================================");
+  console.log("========  Resources Metadata  ========");
+  console.log("======================================");
+  console.log('   ~~~~~~~~~ Header Info ~~~~~~~~~');
+  log(data.headerInfo);
+  console.log('   ~~~~~~ Resources Metadata ~~~~~');
+  log(data.results[0].info);
+  for (var dataItem = 0; dataItem < data.results[0].metadata.length; dataItem++) {
+    console.log("   -------- Resource " + dataItem + " --------");
+    log(data.results[0].metadata[dataItem], {fields: ['ResourceID', 'StandardName', 'VisibleName', 'ObjectVersion']});
+    // outputFields(data.results[0].metadata[dataItem], {fields: ['ResourceID', 'StandardName', 'VisibleName', 'ObjectVersion']});
+  }
+}
+
+async function getClass(resource) {
+  let data = await client.metadata.getClass(resource);
+  console.log("===========================================================");
+  console.log(`========  Class Metadata (from ${resource} Resource)  ========`);
+  console.log("===========================================================");
+  console.log('   ~~~~~~~~~ Header Info ~~~~~~~~~');
+  log(data.headerInfo);
+  console.log('   ~~~~~~~~ Class Metadata ~~~~~~~');
+  log(data.results[0].info);
+  for (let classItem = 0; classItem < data.results[0].metadata.length; classItem++) {
+    console.log("   -------- Table " + classItem + " --------");
+    log(data.results[0].metadata[classItem], {fields: ['ClassName', 'StandardName', 'VisibleName', 'TableVersion']});
+  }
+}
+
+async function getObject(resource) {
+  let data = await client.metadata.getObject(resource);
+  console.log("===========================================================");
+  console.log(`========  Object Metadata (from ${resource} Resource)  ========`);
+  console.log("===========================================================");
+  console.log('   ~~~~~~~~~ Header Info ~~~~~~~~~');
+  log(data.headerInfo);
+  console.log('   ~~~~~~~~ Object Metadata ~~~~~~~');
+  if (data.entriesReceived > 0) {
+    log(data.results[0].info);
+    for (let classItem = 0; classItem < data.results[0].metadata.length; classItem++) {
+      console.log("   -------- Table " + classItem + " --------");
+      log(data.results[0].metadata[classItem], {fields: ['ClassName', 'StandardName', 'VisibleName', 'TableVersion']});
+    }
+  }
+  else
+    console.log('   NO DATA');
+
+}
+
+async function getTableFieldCount(resource, className, classId) {
+  let data = await client.metadata.getTable(resource, classId);
+  console.log(`${resource} ${className} - ${classId}: ${data.results[0].metadata.length} fields`);
+}
+
+async function getTableFields(resource, className, classId) {
+  let data = await client.metadata.getTable(resource, classId);
+  console.log(`${resource} ${className} - ${classId}: ${data.results[0].metadata.length} fields`);
+  console.log("==============================================");
+  console.log(`========  ${resource} ${className} Table Metadata  ========`);
+  console.log("===============================================");
+  console.log('   ~~~~~~~~~ Header Info ~~~~~~~~~');
+  log(data.headerInfo);
+  console.log('   ~~~~~~~~ Table Metadata ~~~~~~~');
+  log(data.results[0].info);
+  console.log('MetadataEntryID, SystemName, ShortName, LongName, DataType');
+  data.results[0].metadata.forEach(field => {
+    console.log(`${field.MetadataEntryID}, ${field.SystemName}, ${field.ShortName}, ${field.LongName}, ${field.DataType}`);
+  });
+}
+
+async function readTable(resource, className, query) {
+  let itemsData = await client.search.query(resource, className, query, {limit:10000, offset:0});
+  itemsData.results.forEach(item => {
+    console.log(`${item.L_ListingID}, ${item.L_State}, ${item.L_City}, ${item.L_AddressStreet}, ${item.L_AddressNumber}, ${item.L_AskingPrice}, ${item.L_PictureCount}`)
+//      if (item.L_PictureCount === 20)
+//        log(item);
+  });
+  console.log(itemsData.results.length);
+}
+
+async function getPhotosForResource(resourceId) {    // get photos
+  let photoResults = await client.objects.getAllObjects("Property", "Photo", resourceId, {alwaysGroupObjects: true, ObjectData: '*'})
+  console.log("=================================");
+  console.log("========  Photo Results  ========");
+  console.log("=================================");
+  console.log('   ~~~~~~~~~ Header Info ~~~~~~~~~');
+  outputFields(photoResults.headerInfo);
+  for (var i = 0; i < photoResults.objects.length; i++) {
+    console.log("   -------- Photo " + (i + 1) + " --------");
+    if (photoResults.objects[i].error) {
+      console.log("      Error: " + photoResults.objects[i].error);
+    } else {
+      outputFields(photoResults.objects[i].headerInfo);
+      fs.writeFileSync(
+        "tmp/photo" + (i + 1) + "." + photoResults.objects[i].headerInfo.contentType.match(/\w+\/(\w+)/i)[1],
+        photoResults.objects[i].data);
+    }
+  }
+}
+
 
 console.log("trying to connect...");
 // establish connection to RETS server which auto-logs out when we're done
-rets.getAutoLogoutClient(clientSettings, function (client) {
+rets.getAutoLogoutClient(clientSettings, async function (client) {
   console.log("===================================");
   console.log("========  System Metadata  ========");
   console.log("===================================");
   console.log('   ~~~~~~~~~ Header Info ~~~~~~~~~');
-  outputFields(client.loginHeaderInfo);
+  log(client.loginHeaderInfo);
   console.log('   ~~~~~~~~~ System Data ~~~~~~~~~');
-  outputFields(client.systemData);
+  log(client.systemData);
 
   //get resources metadata
-  return client.metadata.getResources()
-    .then(function (data) {
-      console.log("======================================");
-      console.log("========  Resources Metadata  ========");
-      console.log("======================================");
-      console.log('   ~~~~~~~~~ Header Info ~~~~~~~~~');
-      outputFields(data.headerInfo);
-      console.log('   ~~~~~~ Resources Metadata ~~~~~');
-      outputFields(data.results[0].info);
-      for (var dataItem = 0; dataItem < data.results[0].metadata.length; dataItem++) {
-        console.log("   -------- Resource " + dataItem + " --------");
-        outputFields(data.results[0].metadata[dataItem], {fields: ['ResourceID', 'StandardName', 'VisibleName', 'ObjectVersion']});
-      }
-    }).then(function () {
 
-      //get class metadata
-      return client.metadata.getClass("Property");
-    }).then(function (data) {
-      console.log("===========================================================");
-      console.log("========  Class Metadata (from Property Resource)  ========");
-      console.log("===========================================================");
-      console.log('   ~~~~~~~~~ Header Info ~~~~~~~~~');
-      outputFields(data.headerInfo);
-      console.log('   ~~~~~~~~ Class Metadata ~~~~~~~');
-      outputFields(data.results[0].info);
-      for (var classItem = 0; classItem < data.results[0].metadata.length; classItem++) {
-        console.log("   -------- Table " + classItem + " --------");
-        outputFields(data.results[0].metadata[classItem], {fields: ['ClassName', 'StandardName', 'VisibleName', 'TableVersion']});
-      }
-    }).then(function () {
+  // get resources
+  await getResources();
 
-      //get field data for open houses
-      return client.metadata.getTable("Property", "RD_1");
-    }).then(function (data) {
-      console.log("==============================================");
-      console.log("========  Residential Table Metadata  ========");
-      console.log("===============================================");
-      console.log('   ~~~~~~~~~ Header Info ~~~~~~~~~');
-      outputFields(data.headerInfo);
-      console.log('   ~~~~~~~~ Table Metadata ~~~~~~~');
-      outputFields(data.results[0].info);
-      for (var tableItem = 0; tableItem < data.results[0].metadata.length; tableItem++) {
-        console.log("   -------- Field " + tableItem + " --------");
-        outputFields(data.results[0].metadata[tableItem], {fields: ['MetadataEntryID', 'SystemName', 'ShortName', 'LongName', 'DataType']});
-      }
-      return data.results[0].metadata
-    }).then(function (fieldsData) {
-      var plucked = [];
-      for (var fieldItem = 0; fieldItem < fieldsData.length; fieldItem++) {
-        plucked.push(fieldsData[fieldItem].SystemName);
-      }
-      return plucked;
-    }).then(function (fields) {
+  // get class metadata
+  await getClass("Property");
+  // await getClass("ActiveAgent");
+  // await getClass("OpenHouse");
 
-      //perform a query using DMQL2 -- pass resource, class, and query, and options
-      return client.search.query("Property", "RD_1", "(LM_Int2_3=0)", {limit:100, offset:10})
-        .then(function (searchData) {
-          console.log("=============================================");
-          console.log("========  Residential Query Results  ========");
-          console.log("=============================================");
-          console.log('   ~~~~~~~~~ Header Info ~~~~~~~~~');
-          outputFields(searchData.headerInfo);
-          console.log('   ~~~~~~~~~~ Query Info ~~~~~~~~~');
-          outputFields(searchData, {exclude: ['results','headerInfo']});
-          //iterate through search results
-          for (var dataItem = 0; dataItem < searchData.results.length; dataItem++) {
-            console.log("   -------- Result " + dataItem + " --------");
-            outputFields(searchData.results[dataItem], {fields: fields});
-          }
-          if (searchData.maxRowsExceeded) {
-            console.log("   -------- More rows available!");
-          }
-        });
-    }).then(function () {
+  // await getObject("Property");
+  // await getObject("ActiveAgent");
+  // await getObject("OpenHouse");
 
-      // get photos
-      return client.objects.getAllObjects("Property", "LargePhoto", photoSourceId, {alwaysGroupObjects: true, ObjectData: '*'})
-    }).then(function (photoResults) {
-      console.log("=================================");
-      console.log("========  Photo Results  ========");
-      console.log("=================================");
-      console.log('   ~~~~~~~~~ Header Info ~~~~~~~~~');
-      outputFields(photoResults.headerInfo);
-      for (var i = 0; i < photoResults.objects.length; i++) {
-        console.log("   -------- Photo " + (i + 1) + " --------");
-        if (photoResults.objects[i].error) {
-          console.log("      Error: " + photoResults.objects[i].error);
-        } else {
-          outputFields(photoResults.objects[i].headerInfo);
-          fs.writeFileSync(
-            "/tmp/photo" + (i + 1) + "." + photoResults.objects[i].headerInfo.contentType.match(/\w+\/(\w+)/i)[1],
-            photoResults.objects[i].data);
-        }
-      }
-    });
+  // await getTableFieldCount("Property", "Residential Detached", "RD_1");
+  // await getTableFieldCount("Property", "Residential Attached", "RA_2");
+  // await getTableFieldCount("Property", "Multifamily", "MF_3");
+  // await getTableFieldCount("Property", "Land", "LD_4");
+  // await getTableFieldCount("ActiveAgent", "Active Agent", "ActiveAgent");
+  // await getTableFieldCount("OpenHouse", "Residential Detached", "RD_1");
+  // await getTableFieldCount("OpenHouse", "Residential Attached", "RA_2");
+  // await getTableFieldCount("OpenHouse", "Multifamily", "MF_3");
+  // await getTableFieldCount("OpenHouse", "Land", "LD_4");
+
+  // await getTableFields("Property", "Residential Detached", "RD_1");
+  // await getTableFields("Property", "Residential Attached", "RA_2");
+  // await getTableFields("Property", "Multifamily", "MF_3");
+  // await getTableFields("Property", "Land", "LD_4");
+  // await getTableFields("ActiveAgent", "Active Agent", "ActiveAgent");
+  // await getTableFields("OpenHouse", "Residential Detached", "RD_1");
+  // await getTableFields("OpenHouse", "Residential Attached", "RA_2");
+  // await getTableFields("OpenHouse", "Multifamily", "MF_3");
+  // await getTableFields("OpenHouse", "Land", "LD_4");
+
+  // await readTable("Property", "RD_1", "(L_UpdateDate=2010-01-01+)");
+  // await readTable("Property", "RA_2", "(L_UpdateDate=2010-01-01+)");
+
+  // await getPhotosForResource('262188347')
 
 }).catch(function (errorInfo) {
   var error = errorInfo? (errorInfo.error || errorInfo): 'unknown';
