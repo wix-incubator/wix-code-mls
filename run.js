6632044a25d7013f089863a36b75ff836abcdc53
yoav@wix.com <yoav@wix.com>
2018-06-20 15:09:33 +0300

    queue and batch support - in dev

--- run.js
+++ run.js
@@ -6,6 +6,7 @@ const syncMLS = require('./lib/sync-mls').syncMLS;
 const makeRetsConfig = require('./lib/sync-mls').makeRetsConfig;
 const tablesConfig = require('./rets.config.json');
 const checkUpdateState = require('./lib/wix-sync').checkUpdateState;
+const batchCheckUpdateState = require('./lib/wix-sync').batchCheckUpdateState;
 const saveItem = require('./lib/wix-sync').saveItem;
 
 // mlsMetadata({
@@ -20,23 +21,24 @@ const saveItem = require('./lib/wix-sync').saveItem;
 //   retsPass: clientSettings.password
 // });
 
-syncMLS({
-    retsUrl: clientSettings.loginUrl,
-    retsUser: clientSettings.username,
-    retsPass: clientSettings.password
-  },
-  credentials,
-  {
-    metasiteId: 'a705d72b-82c5-4920-9ef5-1d8f6c50fd39',
-    userId: 'c569b3d3-4d9b-4196-83ca-bb69f6d5684d'
-  },
-  {
-    secret: 'xxxx-xxxx-xxxx',
-    checkUpdateState: 'https://yoav68.wixsite.com/mls-rets/_functions-dev/checkUpdateState',
-    saveItem: 'https://yoav68.wixsite.com/mls-rets/_functions-dev/saveItem'
-  },
-  tablesConfig
-);
+// syncMLS({
+//     retsUrl: clientSettings.loginUrl,
+//     retsUser: clientSettings.username,
+//     retsPass: clientSettings.password
+//   },
+//   credentials,
+//   {
+//     metasiteId: 'a705d72b-82c5-4920-9ef5-1d8f6c50fd39',
+//     userId: 'c569b3d3-4d9b-4196-83ca-bb69f6d5684d'
+//   },
+//   {
+//     secret: 'xxxx-xxxx-xxxx',
+//     checkUpdateState: 'https://yoav68.wixsite.com/mls-rets/_functions-dev/checkUpdateState',
+//     saveItem: 'https://yoav68.wixsite.com/mls-rets/_functions-dev/saveItem'
+//   },
+//   tablesConfig
+// );
+
 
 // saveItem({data: 1, name: 'wefwe', date: new Date()},
 //   {
@@ -63,3 +65,19 @@ syncMLS({
 //     console.log(res)
 // })
 //   .catch(e => {console.log('error', e)});
+
+batchCheckUpdateState([{id: '262180216', L_ListingDate: new Date()},
+    {id: '262178254', L_ListingDate: new Date(new Date().getTime() - 1000000000000)},
+    {id: '000', L_ListingDate: new Date()}],
+  {
+    secret: 'xxxx-xxxx-xxxx',
+    checkUpdateState: 'https://yoav68.wixsite.com/mls-rets/_functions-dev/batchCheckUpdateState',
+    syncItem: ''
+  },
+  "Properties",
+  "id",
+  "L_ListingDate"
+  ).then(res => {
+    console.log(res)
+})
+  .catch(e => {console.log('error', e)});
