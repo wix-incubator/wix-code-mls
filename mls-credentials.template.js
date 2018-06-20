a231fe897b7d1ca278f070d63cbffbd55c1ce9e1
yoav@wix.com <yoav@wix.com>
2018-06-20 13:50:16 +0300

    added template credentials file

---
+++ mls-credentials.template.js
@@ -0,0 +1,8 @@
+module.exports.clientSettings = {
+  loginUrl: 'http://xxxx/xxxx',
+  username: 'xxx',
+  password: 'xxx',
+  version: 'RETS/1.7.2',
+  userAgent: 'RETS node-client/4.x',
+  method: 'GET'  // this is the default, or for some servers you may want 'POST'
+};
--- run.js
+++ run.js
@@ -1,4 +1,4 @@
-const clientSettings = require("./josh-credentials").clientSettings;
+const clientSettings = require("./mls-josh-credentials").clientSettings;
 const credentials = require('./media-manager-credentials').credentials;
 
 const mlsMetadata = require('./lib/sync-mls').mlsMetadata;
