7740fb4eca6eafcc12692bb527070406b8fc1bc4
yoav@wix.com <yoav@wix.com>
2018-06-30 05:53:14 +0300

    change algorithm to use hash of item to decide if to update

--- run.js
+++ run.js
@@ -35,6 +35,7 @@ syncMLS({
     secret: 'xxxx-xxxx-xxxx',
     checkUpdateState: 'https://yoav68.wixsite.com/mls-rets/_functions-dev/checkUpdateState',
     batchCheckUpdateState: 'https://yoav68.wixsite.com/mls-rets/_functions-dev/batchCheckUpdateState',
+    batchCheckUpdateState2: 'https://yoav68.wixsite.com/mls-rets/_functions-dev/batchCheckUpdateState2',
     saveItem: 'https://yoav68.wixsite.com/mls-rets/_functions-dev/saveItem'
   },
   tablesConfig
